a85018076fc4fa64eed1e38c57abe26497f5146d
Kazuhiro Serizawa <nserihiro@gmail.com>
2019-01-30 22:24:43 +0900

    add embarassingly_parallel version for dgemv

--- dgemv/util.c
+++ dgemv/util.c
@@ -1,7 +1,14 @@
 #include "util.h"
+#include <stdio.h>
 
 void assert_result(int n, double *v, double expected)
 {
     for (int i = 0; i < n; ++i)
+    {
+        if (v[i] != expected)
+        {
+            printf("%d, %f\n", i, v[i]);
+        }
         assert(v[i] == expected);
+    }
 }
